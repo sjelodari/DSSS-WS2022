39544eb079131778c4ba18e3781058d8e8c38eff
Saber <saber.jelodarii@gmail.com>
2022-12-04 16:13:35 +0100

    adding instructions

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+set(
+    name="snowflake",
+    version="0.1",
+    author="Saber",
+    author_email="saber.jelodari@fau.de",
+    pakages=find_packages(),
+    install_requires=["numpy","turtles"],
+)
